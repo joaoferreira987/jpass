7d0a5a10e7e11b269b995eef53d1f39d25874d50
Joao Ferreira <joaoluisazevedoferreira@gmail.com>
2022-10-06 20:19:40 +0100

    Added unit tests for sha-256 hash function

--- src/test/java/jpass/util/CryptUtilsTest.java
+++ src/test/java/jpass/util/CryptUtilsTest.java
@@ -8,13 +8,61 @@ public class CryptUtilsTest {
     //According to category-partition....
 
     @Test
-    public void testCase1(){
+    public void testNullCharacters(){
+
+        //arrange
+        String null_string = null;
+
+        //act
+        String result = getSha256Hash(null_string);
+
+        //assert
+        org.junit.Assert.assertNull(result);
+    
     }
 
     @Test
-    public void testCase2(){
+    public void testMorethan64chars(){
+
+        //arrange
+        String text = "abcedfghijklmnopqrstuvwxyz1234567890abcedfghijklmnopqrstuvwxyz1234567890"
+
+        //act
+        String result = getSha256Hash(text);
+
+        //assert
+        org.junit.Assert.assertTrue(result.length == 64);
+
+    }
+
+    @Test
+    public void testLessthan64chars(){
+
+         //arrange
+        String text = "abcedfghijklmnopqrstuvwxyz1234567890"
+
+        //act
+        String result = getSha256Hash(text);
+
+        //assert
+        org.junit.Assert.assertTrue(result.length == 64);
+
+
+    }
+
+    @Test
+    public void testCorrectHash(){
+
+        //arrange
+        String text = "string"
+
+        //act
+        String result = getSha256Hash(text);
+
+        //assert
+        org.junit.Assert.assertEquals("473287f8298dba7163a897908958f7c0eae733e25d2e027992ea2edc9bed2fa8", result);
 
     }
     
-    //...
+   
 }
