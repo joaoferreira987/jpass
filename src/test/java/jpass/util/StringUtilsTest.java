74355b743ebc8c11c906c5c2b553863d6ca43234
joao <joaoluisazevedoferreira@gmail.com>
2022-10-06 15:28:00 +0100

    Done test caes for StringStrip function

--- src/test/java/jpass/util/StringUtilsTest.java
+++ src/test/java/jpass/util/StringUtilsTest.java
@@ -5,15 +5,87 @@ import org.junit.Test;
 
 public class StringUtilsTest {
 
-    //According to category-partition....
+    
+    /**
 
+        TestCase1()
+        If length is bigger than the provided string, then the function should return the original string
+    
+     */
     @Test
     public void testCase1(){
+        
+        //arrange
+        String text = "String"; 
+        int length = 7;
+        
+        //act
+        String result = StringUtils.stripString(text,length);
+    
+        //assert
+        org.junit.Assert.assertEquals("String", result);
+    
     }
 
+     /**
+
+        testCase2()
+        If length is smaller than the provided string, then the function should return the correct substring
+    
+     */
     @Test
     public void testCase2(){
+        
+        //arrange
+        
+        String text = "String"; 
+        int length = 2;
+        
+        //act
+        String result = StringUtils.stripString(text,length);
+    
+        //assert
+        org.junit.Assert.assertEquals("St", result);
+    }
 
+    /**
+        testNegativeLength()
+        If the provided length is null, the function should return the provided string object
+    */
+
+    @Test
+    public void testNegativeLength(){
+
+        //arrange
+        String text = "String";
+        int length = -1;
+
+        //act
+        String result = StringUtils.stripString(text,length);
+        
+         //assert
+        org.junit.Assert.assertEquals("String",result);
+
+    }
+
+    
+
+    /**
+        testNullString()
+        If the provided string is null, the function should return null
+     */
+    @Test 
+    public void testNullString(){
+        
+        //arrange
+        String text = null;
+        int length = 3; //doesnt matter
+
+        //act
+        String result = StringUtils.stripString(text,length);
+
+        //assert
+        org.junit.Assert.assertNull(result);
     }
     
     //...
